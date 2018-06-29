b06f12552cded4a961a066eae8e80108be0d48cb
github username <github email address>
2018-06-29 15:58:17 -0400

    coded promises

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion() {
+  document.getElementsByClassName("question-container")[0].innerHTML = questions[0].questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  let promise = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+  return promise
+}
+
+function removeQuestion() {
+  document.getElementsByClassName("question-container")[0].innerHTML = ""
+}
+
+function askQuestionThenRemoveQuestion() {
+  appendQuestion()
+  let ask = new Promise((resolve, reject) => setTimeout(removeQuestion, 10000))
+  return ask
+}
+
+function trueAndFalseButtons() {
+  let buttons = [document.querySelector(".green"), document.querySelector(".red")]
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+  const buttons = trueAndFalseButtons()
+  for ( const button of trueAndFalseButtons()) {
+    button.classList.contains("hide") ? button.classList.remove("hide") : button.classList.add("hide")
+  }
+}
+
+function displayQuestionOnClick() {
+  for ( const button of trueAndFalseButtons()) {
+    button.addEventListener('click', askQuestionThen)
+  }
+}
